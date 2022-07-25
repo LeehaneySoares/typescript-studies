c9e1ac36c7dd643816fdd3b737c8039e28e8db64
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-25 11:52:03 -0300

    chore(ts): compilando um js sem erros de tipo

--- aula-01/src/script.ts
+++ aula-01/src/script.ts
@@ -1,12 +1,12 @@
 const n1 = document.getElementById('n1') as HTMLInputElement
 const n2 = document.getElementById('n2') as HTMLInputElement
-const button = document.getElementById('calcular')
-const result = document.getElementById('res')
+const button = document.getElementById('calcular') as HTMLElement
+const result = document.getElementById('res') as HTMLElement
 
-function calcular (n1, n2) {
+function calcular (n1: number, n2: number) {
   return n1 + n2
 }
 
 button.addEventListener('click', function () {
-  result.innerHTML = calcular(n1.value, n2.value)
+  result.innerHTML = calcular(Number(n1.value), Number(n2.value)).toString()
 })
