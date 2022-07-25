0623d3dbe5fc02483114d33cc46e1cd5c25c427d
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-25 16:07:21 -0300

    chore(ts): aprendendo sobre as tipagems primitívas

---
+++ aula-02/src/script.ts
@@ -0,0 +1,17 @@
+/**
+ * Types primitivos
+ */
+
+let nome: string = 'Leehaney'
+let idade: number = 25
+let programador: boolean = true
+
+/**
+ * Types arrays
+ */
+
+const idades: Array<number> = [20, 3, 2, 1, 56, 43, 49, 23 ]
+const idades2: number[] = [20, 3, 2, 1, 56, 43, 49, 23 ]
+
+const nomes: Array<string> = ['Leehaney', 'Valmira', 'Betania']
+const nomes2: string[] = ['Leehaney', 'Valmira', 'Betania']
