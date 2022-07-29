b8848e7dc18c4a981e42b0cfcfdfac04f6573dbd
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-07-29 08:57:32 -0300

    chore(ts): aprendendo como receber props opcionais

---
+++ aula-05/src/script.ts
@@ -0,0 +1,23 @@
+// function criaPessoa (
+//   pessoa: {
+//     nome: string,
+//     sobrenome: string,
+//     idade: number,
+//     estadoCivil: string,
+//     filhos: Array<string>
+//   }
+// ): string {
+//   let { nome, sobrenome, idade, estadoCivil, filhos } = pessoa
+//   return `${nome} ${sobrenome} tem ${idade} anos, é ${estadoCivil} e tem ${filhos.length} filhos.`
+// }
+
+
+// const pessoa = {
+//     nome: 'Leehaney',
+//     sobrenome: 'Soares',
+//     idade: 25,
+//     estadoCivil: 'Casado',
+//     filhos: ['Livia, joaquim', 'cleiton', 'michele']
+// 	}
+
+// const p1: string = criaPessoa(pessoa)
---
+++ aula-06/script.ts
@@ -0,0 +1,13 @@
+function usuario (pessoa: { nome: string, sobrenome: string, idade?: number }): string {
+  return pessoa.idade
+    ? `${pessoa.nome} ${pessoa.sobrenome} tem ${pessoa.idade} anos.`
+    : `${pessoa.nome} ${pessoa.sobrenome}, tudo bem ?`
+}
+
+const pessoa = {
+  nome: 'Leehaney',
+  sobrenome: 'Soares',
+  idade: 25 // É uma propriedade opcional
+}
+
+usuario(pessoa)
