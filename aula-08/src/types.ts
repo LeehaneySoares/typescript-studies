6c4ff6090afc507d5d73fa0527c99ac172c1beac
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-08-01 13:02:19 -0300

    chore(ts): aprendendo sobre types e interfaces

---
+++ aula-08/src/types.ts
@@ -0,0 +1,29 @@
+/**
+ * Types são imutáveis, ao definir as props em seu corpo,
+ * não podemos em outros lugares modifica-los, teremos que retornar aqui para realizar
+ * uma nova inserção ou remoção
+ */
+
+type User = {
+  nome: string,
+  idade: number,
+  endereco: string,
+  documentos: Array<string>,
+  telefones: Array<number>
+}
+
+function mostraUsuario (usuario: User): string {
+  return `
+    ${usuario.nome} mora em: ${usuario.endereco}, tendo ${usuario.idade} anos.
+    Dono dos seguintes documentos: ${usuario.documentos} 
+    com os seguintes telefones: ${usuario.telefones.join(', ')}
+  `
+}
+
+mostraUsuario({
+  nome: 'Leehaney',
+  idade: 25,
+  endereco: 'Av. Arquiteto Vilanova Artigas',
+  documentos: ['RG', 'CPF', 'CNH'],
+  telefones: [11940215919, 11982455840, 1193468477]
+})
