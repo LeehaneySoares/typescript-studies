be81586f3e913f80d6cb268b83301cb1fba568e2
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-08-02 10:08:58 -0300

    chore(ts): aprendendo a utilizar types literals em variáveis

---
+++ aula-10/variavel.ts
@@ -0,0 +1,9 @@
+/**
+ * Types literals em variáveis
+ */
+
+let nome: 'Leehaney' = 'Leehaney'
+
+nome = 'Testando' // O conteúdo esperado é do tipo 'Leehaney' e não 'Testando'
+
+nome = 'Leehaney' // Aqui funciona!
