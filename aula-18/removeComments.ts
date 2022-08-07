4ede4424bf37cd087e494bd5f70f509a50790179
Leehaney Soares Ferreira Nazaré <leehaney.soares@gmail.com>
2022-08-07 18:33:55 -0300

    chore(ts): aprendendo sobre removeComments, emit, noEmit, noEmitOnError, noUnusedLocals e noUnusedParameters

--- aula-18/removeComments.ts
+++ aula-18/removeComments.ts
@@ -5,3 +5,25 @@
  */
 
 const teste: string = 'Esse arquivo está comentado'
+
+/**
+ * noUnusedLocals: Emite um aviso quando declararmos uma variável em um escopo local
+ * e não utilizamos ela.
+ */
+
+let outroNome: string = 'Leehaney'
+
+function soma (n1: number, n2: number): number {
+  let nome: string = 'Leehaney'
+  return n1 + n2
+}
+
+/**
+ * noUnusedParameters: É similar ao noUnusedLocals, porém com os parâmetros. Caso declararmos
+ * parâmetros em uma função e não estamos utilizando todos, ele irá emitir um aviso e não compilará
+ * nosso código caso estivermos com o noEmitOnError ativado!
+ */
+
+function sub (n1: number, n2: number, n3: number): number {
+  return n1 - n2
+}
